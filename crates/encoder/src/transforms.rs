a30b6159eaa22020470229711868054b931c69b2
Rong Ma <160457111+rongma7@users.noreply.github.com>
2024-04-18 16:42:07 +0800

    add encoder KZG proof

--- crates/encoder/src/transforms.rs
+++ crates/encoder/src/transforms.rs
@@ -1,12 +1,13 @@
-use std::mem;
+use std::sync::mpsc::SendError;
 
 use crate::types::*;
 use ark_ec::{bn::Bn, AffineRepr, CurveGroup, VariableBaseMSM};
-use ark_std::iterable::Iterable;
+use ark_std::{iterable::Iterable, Zero};
 use ethereum_types::H256;
 use ark_bn254::{Fr as Scalar, G1Affine, G1Projective};
-use ark_ff::{fields::Field, MontConfig};
+use ark_ff::{fields::Field, MontConfig, PrimeField};
 use ark_poly::{EvaluationDomain, Radix2EvaluationDomain};
+use ark_serialize::{CanonicalSerialize, SerializationError};
 //use once_cell::sync::Lazy;
 // use transpose::transpose;
 
@@ -16,114 +17,119 @@ fn raw_unit_to_scalar(chunk: &[u8]) -> Scalar {
     <Scalar as Field>::from_random_bytes(&chunk).unwrap() // 33 ms
 }
 
-pub fn raw_data_to_raw_blob(data: &RawData) -> Result<RawBlob, String> {
-    let raw_blob_1d: Vec<_> = data.chunks(RAWUNIT).map(raw_unit_to_scalar).collect();
-    let raw_blob_1d: Box<[Scalar; BLOBN * BLOBN]> = raw_blob_1d.into_boxed_slice().try_into().unwrap();
-    
-    if mem::size_of::<[Scalar; BLOBN * BLOBN]>() != mem::size_of::<[[Scalar; BLOBN]; BLOBN]>() || mem::align_of::<[Scalar; BLOBN * BLOBN]>() != mem::align_of::<[[Scalar; BLOBN]; BLOBN]>()
-    {
-        //compile_error!("Invalid unsafe transmute in raw_data_to_raw_blob().");
-        return Err(format!("Invalid unsafe transmute in raw_data_to_raw_blob()."));
-    }
-    let raw_blob_2d: RawBlob = unsafe {
-        std::mem::transmute(raw_blob_1d)
-    };
-    Ok(raw_blob_2d)
-}
-
-pub fn single_fft(coeffs: &mut Vec<Scalar>, fft_domain: Radix2EvaluationDomain::<Scalar>) -> () {
-    fft_domain.fft_in_place(coeffs);
-}
-
-pub fn single_ifft(evals: &mut Vec<Scalar>, ifft_domain: Radix2EvaluationDomain::<Scalar>) -> () {
-    ifft_domain.ifft_in_place(evals);
-}
-
-pub fn single_rs_encode(evals: &mut Vec<Scalar>, ifft_domain: Radix2EvaluationDomain::<Scalar>, fft_domain: Radix2EvaluationDomain::<Scalar>) -> () {
-    single_ifft(evals, ifft_domain);
-    single_fft(evals, fft_domain);
+pub fn raw_data_to_raw_blob(data: &RawData) -> RawBlob {
+    let raw_blob_1d: Vec<_> = data.chunks(RAW_UNIT).map(raw_unit_to_scalar).collect();
+    RawBlob(raw_blob_1d)
 }
 
 // rs_encode for each row of matrix (in place)
 pub fn rs_encode_blobn_cols(matrix: &mut Vec<Vec<Scalar>>) -> () {
-    // all rows use the same ifft_domain and the same fft_domain
-    let ifft_domain = Radix2EvaluationDomain::<Scalar>::new(BLOBN).unwrap();
-    let fft_domain = Radix2EvaluationDomain::<Scalar>::new(BLOBN << 1).unwrap();
+    // all rows use the same domain_blob_row_n and the same domain_blob_row_n2
+    let domain_blob_row_n = Radix2EvaluationDomain::<Scalar>::new(BLOB_ROW_N).unwrap();
+    let domain_blob_row_n2 = Radix2EvaluationDomain::<Scalar>::new(BLOB_ROW_N2).unwrap();
     // RS encode for each row of matrix
     matrix.iter_mut().for_each(|column_data| {
-        single_rs_encode(column_data, ifft_domain, fft_domain);
+        domain_blob_row_n.ifft_in_place(column_data);
+        domain_blob_row_n2.fft_in_place(column_data);
     });
 }
 
 /// RS encode for each col of raw_blob
-pub fn raw_blob_to_encoded_blob_encode(raw_blob: &RawBlob) -> Result<EncodedBlobEncoded, String> {
+pub fn raw_blob_to_encoded(raw_blob: &RawBlob) -> EncodedBlobEncoded {
     // transpose raw_blob
-    let mut raw_blob_transpose: Vec<Vec<Scalar>> = (0..BLOBN)
-        .map(|col_index| raw_blob.iter().map(|row| row[col_index].clone()).collect::<Vec<Scalar>>())
+    let mut raw_blob_transpose: Vec<Vec<Scalar>> = (0..BLOB_COL_N)
+        .map(|col_index| (0..BLOB_ROW_N).map(|row_index| raw_blob[(row_index << BLOB_COL_LOG) + col_index].clone()).collect::<Vec<Scalar>>())
         .collect();
     // RS encode for each row of raw_blob_transpose, i.e., for each col of raw_blob
     rs_encode_blobn_cols(&mut raw_blob_transpose);
     // transpose back and flat
-    let encoded_flatten: Vec<Scalar> = (0..BLOBN << 1)
+    let encoded_flatten: Vec<Scalar> = (0..BLOB_ROW_N << 1)
         .flat_map(|row_index| raw_blob_transpose.iter().map(|col| col[row_index].clone()).collect::<Vec<Scalar>>())
         .collect();
-    // vec_1d to Box_2d
-    let encoded_flatten: Box<[Scalar; BLOBN * BLOBN * 2]> = encoded_flatten.into_boxed_slice().try_into().unwrap();
-    if mem::size_of::<[Scalar; BLOBN * BLOBN * 2]>() != mem::size_of::<[[Scalar; BLOBN]; BLOBN * 2]>() || mem::align_of::<[Scalar; BLOBN * BLOBN * 2]>() != mem::align_of::<[[Scalar; BLOBN]; BLOBN * 2]>()
-    {
-        //compile_error!("Invalid unsafe transmute in raw_blob_to_encoded_blob_encode().");
-        return Err(format!("Invalid unsafe transmute in raw_blob_to_encoded_blob_encode()."));
-    }
-    let encoded: EncodedBlobEncoded = unsafe {
-        std::mem::transmute(encoded_flatten)
-    };
-    Ok(encoded)
+    EncodedBlobEncoded(encoded_flatten)
 }
 
-pub fn single_commit(_evals: &[Scalar; BLOBN], ifft_domain: Radix2EvaluationDomain::<Scalar>, setup_g1: &SimulateSetupG1) -> G1Affine {
+pub fn coeffs_to_commitment(coeffs: &Vec<Scalar>, setup_g1: &SimulateSetupG1) -> G1Affine {
+    let coeffs_repr: Vec<_> = coeffs.iter().map(|x| MontConfig::into_bigint(*x)).collect();
+    G1Projective::msm_bigint(&setup_g1[..coeffs_repr.len()], &coeffs_repr).into_affine()
+}
+
+pub fn evals_to_commitment(_evals: &[Scalar], domain: Radix2EvaluationDomain::<Scalar>, setup_g1: &SimulateSetupG1) -> G1Affine {
     let mut evals = _evals.to_vec();
-    single_ifft(&mut evals, ifft_domain);
-    let coeffs_repr: Vec<_> = evals.into_iter().map(|x| MontConfig::into_bigint(x)).collect();
-    G1Projective::msm_bigint(setup_g1, &coeffs_repr).into_affine()
+    domain.ifft_in_place(&mut evals);
+    coeffs_to_commitment(&evals, setup_g1)
 }
 
 /// KZG commit for each row of raw_blob
-pub fn raw_blob_to_encoded_blob_commit(raw_blob: &RawBlob, setup_g1: &SimulateSetupG1) -> RowCommitments {
-    // all rows use the same ifft_domain
-    let ifft_domain = Radix2EvaluationDomain::<Scalar>::new(BLOBN).unwrap();
+pub fn encoded_to_row_commitments(encoded: &EncodedBlobEncoded, setup_g1: &SimulateSetupG1) -> RowCommitments {
+    // all rows use the same domain_blob_col_n
+    let domain_blob_col_n = Radix2EvaluationDomain::<Scalar>::new(BLOB_COL_N).unwrap();
     // KZG commitment
-    let row_commitments: Vec<_> = raw_blob
-        .iter()
-        .map(|row| single_commit(&row, ifft_domain, setup_g1))
+    let row_commitments: Vec<_> = encoded
+        .chunks(BLOB_COL_N)
+        .map(|row| evals_to_commitment(row, domain_blob_col_n, setup_g1))
         .collect();
-    let row_commitments: [G1Affine; BLOBN] = row_commitments.try_into().unwrap();
     RowCommitments(row_commitments)
 }
 
-
-pub fn rs_encode_blobn_cols_g1(row_commitments: &RowCommitments) -> RowCommitmentsEncoded{
-    let mut evals_g1: Vec<_> = row_commitments.iter().map(|x| x.into_group()).collect();
-    let ifft_domain = Radix2EvaluationDomain::<Scalar>::new(BLOBN).unwrap();
-    let fft_domain = Radix2EvaluationDomain::<Scalar>::new(BLOBN << 1).unwrap();
-    ifft_domain.ifft_in_place(&mut evals_g1);
-    fft_domain.fft_in_place(&mut evals_g1);
-    let row_commitments_encoded: [G1Affine; BLOBN << 1] = evals_g1
-        .iter()
-        .map(|x| x.into_affine())
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
-    RowCommitmentsEncoded(row_commitments_encoded)
+pub fn g1_to_scalar(g1: &G1Affine) -> Result<Scalar, SerializationError> {
+    let mut compressed_bytes = Vec::new();
+    g1.serialize_compressed(&mut compressed_bytes)?;
+    Ok(<Scalar as PrimeField>::from_le_bytes_mod_order(&compressed_bytes))
 }
 
-pub fn raw_blob_to_encoded_blob(raw_blob: &RawBlob, setup_g1: &SimulateSetupG1) -> Result<(), String> { //EncodedBlob {
-    // RS encode for each col of raw_blob
-    let encoded: EncodedBlobEncoded = raw_blob_to_encoded_blob_encode(raw_blob).unwrap();
-    // commitment for each row of raw_blob
-    let row_commitments = raw_blob_to_encoded_blob_commit(raw_blob, setup_g1);
-    // RS encode for row_commitments
-    let row_commitments_encoded = rs_encode_blobn_cols_g1(&row_commitments);
-    Ok(())
+pub fn coeffs_to_multi_proof(coeffs: &Vec<Scalar>, setup_g1: &SimulateSetupG1) -> Vec<G1Affine> {
+    // f_i: coeffs[i], m = BLOB_ROW_N
+    // left vector: f_{m-1}, 0, ..., 0 (totally m-1 0's), f_{m-1}, f_0, f_1, ..., f_{m-2}
+    // right vector: setup_g1[m-1], ..., setup_g1[0], 0, ..., 0 (totally m G1 0's)
+    // ifft(fft(left vector) element-wise-product fft(right vector)), take the first m elements as h
+    // result = fft(h)
+    let domain_blob_row_n = Radix2EvaluationDomain::<Scalar>::new(BLOB_ROW_N).unwrap();
+    let domain_blob_row_n2 = Radix2EvaluationDomain::<Scalar>::new(BLOB_ROW_N2).unwrap();
+
+    let mut right: Vec<G1Projective> = setup_g1[..BLOB_ROW_N].iter().map(|x| x.clone().into_group()).collect();
+    right.reverse();
+    right.extend(vec![G1Projective::zero(); BLOB_ROW_N]);
+    domain_blob_row_n2.fft_in_place(&mut right);
+
+    let mut left: Vec<Scalar> = vec![Scalar::zero(); BLOB_ROW_N + 1];
+    left[0] = coeffs[BLOB_ROW_N - 1].clone();
+    left[BLOB_ROW_N] = coeffs[BLOB_ROW_N - 1].clone();
+    left.extend(coeffs[..BLOB_ROW_N - 1].to_vec());
+    domain_blob_row_n2.fft_in_place(&mut left);
+    
+    let mut h: Vec<G1Projective> = right.iter().zip(left.iter())
+        .map(|(iright, ileft)| *iright * ileft)
+        .collect();
+    domain_blob_row_n2.ifft_in_place(&mut h);
+    h.truncate(BLOB_ROW_N); // here h is h
+    domain_blob_row_n.fft_in_place(&mut h);
+    h.into_iter().map(|x| x.into_affine()).collect::<Vec<G1Affine>>()
+}
+
+pub fn encoded_to_KZG(encoded: EncodedBlobEncoded, setup_g1: &SimulateSetupG1) -> Result<EncodedBlobKZG, String> { //EncodedBlob {
+    // KZG
+    if setup_g1.len() < std::cmp::max(BLOB_COL_N, BLOB_ROW_N2) {
+        return Err(format!("The degree of setup_g1 {} is less than required {}", setup_g1.len(), std::cmp::max(BLOB_COL_N, BLOB_ROW_N2)));
+    }
+    // commit for each row of encoded
+    let row_commitments = encoded_to_row_commitments(&encoded, setup_g1);
+    // commit for row_commitments
+    let mut row_commitments_scalars = row_commitments
+        .iter()
+        .map(|g1| g1_to_scalar(g1))
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|err| format!("SerializationError in g1_to_scalar(), err = {}", err))?;
+    let domain_blob_row_n2 = Radix2EvaluationDomain::<Scalar>::new(BLOB_ROW_N2).unwrap();
+    domain_blob_row_n2.ifft_in_place(&mut row_commitments_scalars);
+    let da_commitment = coeffs_to_commitment(&row_commitments_scalars, setup_g1);
+    let da_proofs = coeffs_to_multi_proof(&row_commitments_scalars, setup_g1);
+    Ok(EncodedBlobKZG {
+        encoded,
+        row_commitments,
+        da_commitment,
+        da_proofs
+    })
     //raw_blob
 }
 
@@ -133,11 +139,26 @@ mod tests {
     use std::time::Instant;
     use rand::{rngs::StdRng, Rng, SeedableRng};
     use test_case::test_case;
+    use ark_bn254::{Fr as Scalar, G1Affine};
+    use ark_ff::{Field, MontConfig, UniformRand};
+    use ark_std::rand::thread_rng;
+    #[test]
+    fn test_bytes_to_scalar() {
+        let chunk = [0u8; 31];
+        let scalar = <Scalar as Field>::from_random_bytes(&chunk).unwrap();
+        dbg!(MontConfig::into_bigint(scalar));
+    }
+    #[test]
+    fn test_g1_to_scalar() {
+        let mut rng = thread_rng();
+        let g1 = G1Affine::rand(&mut rng);
+        let scalar = g1_to_scalar(&g1);
+    }
     #[test_case(0 => Ok(()); "zero sized data")]
     #[test_case(1 => Ok(()); "one sized data")]
     #[test_case(12345565 => Ok(()); "normal sized data")]
-    #[test_case(RAWUNIT * BLOBN * BLOBN => Ok(()); "exact sized data")]
-    #[test_case(RAWUNIT * BLOBN * BLOBN + 1 => Err(format!("Input byte slice length {} exceeds the required length {} for RawData.", RAWUNIT * BLOBN * BLOBN + 1, RAWUNIT * BLOBN * BLOBN)); "overflow sized data")]
+    #[test_case(RAW_UNIT * BLOB_ROW_N * BLOB_COL_N => Ok(()); "exact sized data")]
+    #[test_case(RAW_UNIT * BLOB_ROW_N * BLOB_COL_N + 1 => Err(format!("Input byte slice length {} exceeds the required length {} for RawData.", RAW_UNIT * BLOB_ROW_N * BLOB_COL_N + 1, RAW_UNIT * BLOB_ROW_N * BLOB_COL_N)); "overflow sized data")]
     fn benchmark_batcher(num_bytes: usize) -> Result<(), String> {
         // generate random original data with {num_bytes} bytes
         let seed = 22u64;
@@ -145,23 +166,29 @@ mod tests {
         let mut data = vec![0u8; num_bytes];
         rng.fill(&mut data[..]);
 
-        // padding original data to raw_data
+        // zero-padding original data to raw_data
         let raw_data: RawData = RawData::try_from(data.as_slice())?;
         
         // raw_data_to_raw_blob
         let start = Instant::now();
-        let raw_blob = raw_data_to_raw_blob(&raw_data).unwrap();
+        let raw_blob = raw_data_to_raw_blob(&raw_data);
         let duration = start.elapsed().as_millis();
         println!("Time taken for raw_data_to_raw_blob: {:?}ms with original {} bytes and raw_data {} bytes", duration, num_bytes, raw_data.len());
         
-        // obtain setup_g1
-        let setup_g1 = SimulateSetupG1::sim_load();
-        // raw_blob_to_encoded_blob
+        // encode
         let start = Instant::now();
-        let encoded_blob = raw_blob_to_encoded_blob(&raw_blob, &setup_g1).unwrap();
+        let encoded: EncodedBlobEncoded = raw_blob_to_encoded(&raw_blob);
         let duration = start.elapsed().as_millis();
-        println!("Time taken for raw_blob_to_encoded_blob: {:?}ms with raw_data {} bytes", duration, raw_data.len());
+        println!("Time taken for RS_encode: {:?}ms with raw_data {} bytes", duration, raw_data.len());
+
+        // KZG
+        let setup_g1 = SimulateSetupG1::sim_load();
+        let start = Instant::now();
+        let encoded_blob_KZG = encoded_to_KZG(encoded, &setup_g1).unwrap();
+        let duration = start.elapsed().as_millis();
+        println!("Time taken for encoded_to_KZG: {:?}ms with raw_data {} bytes", duration, raw_data.len());
         
+        // Merkle
         Ok(())
     }
 }
--- crates/encoder/src/types.rs
+++ crates/encoder/src/types.rs
@@ -6,22 +6,25 @@ use ark_poly::{EvaluationDomain, Radix2EvaluationDomain};
 use ethereum_types::H256;
 use ark_bn254::{Fr as Scalar, G1Affine, G1Projective};
 
-pub const RAWUNIT: usize = 31;
-pub const BLOBUNIT: usize = 32;
-pub const BLOBNLOG: usize = 10;
-pub const BLOBN: usize = 1 << BLOBNLOG;
+pub const RAW_UNIT: usize = 31;
+pub const BLOB_UNIT: usize = 32;
+pub const BLOB_ROW_LOG: usize = 10;
+pub const BLOB_COL_LOG: usize = 11; // TODO
+pub const BLOB_ROW_N: usize = 1 << BLOB_ROW_LOG;
+pub const BLOB_ROW_N2: usize = BLOB_ROW_N << 1;
+pub const BLOB_COL_N: usize = 1 << BLOB_COL_LOG;
 
 #[repr(transparent)]
-pub struct RawData(Box<[u8; RAWUNIT * BLOBN * BLOBN]>);
+pub struct RawData(Vec<u8>); // RAW_UNIT * BLOB_ROW_N * BLOB_COL_N
 
 impl Default for RawData {
     fn default() -> Self {
-        RawData(vec![0u8; RAWUNIT * BLOBN * BLOBN].into_boxed_slice().try_into().unwrap())
+        RawData(vec![0u8; RAW_UNIT * BLOB_ROW_N * BLOB_COL_N])
     }
 }
 
 impl Deref for RawData {
-    type Target = Box<[u8; RAWUNIT * BLOBN * BLOBN]>;
+    type Target = Vec<u8>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -38,22 +41,22 @@ impl TryFrom<&[u8]> for RawData {
     type Error = String;
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         let value_len = value.len();
-        if value_len <= RAWUNIT * BLOBN * BLOBN {
-            let mut array = Box::new([0u8; RAWUNIT * BLOBN * BLOBN]);
+        if value_len <= RAW_UNIT * BLOB_ROW_N * BLOB_COL_N {
+            let mut array = vec![0u8; RAW_UNIT * BLOB_ROW_N * BLOB_COL_N];
             array[..value_len].copy_from_slice(&value[..]);
             Ok(RawData(array))
         } else {
-            Err(format!("Input byte slice length {} exceeds the required length {} for RawData.", value.len(), RAWUNIT * BLOBN * BLOBN))
+            Err(format!("Input byte slice length {} exceeds the required length {} for RawData.", value.len(), RAW_UNIT * BLOB_ROW_N * BLOB_COL_N))
         }
     }
 }
 
 
 #[repr(transparent)]
-pub struct RawBlob(Box<[[Scalar; BLOBN]; BLOBN]>);
+pub struct RawBlob(pub Vec<Scalar>); // BLOB_ROW_N * BLOB_COL_N
 
 impl Deref for RawBlob {
-    type Target = Box<[[Scalar; BLOBN]; BLOBN]>;
+    type Target = Vec<Scalar>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -67,10 +70,10 @@ impl DerefMut for RawBlob {
 }
 
 #[repr(transparent)]
-pub struct EncodedBlobEncoded(Box<[[Scalar; BLOBN]; BLOBN * 2]>);
+pub struct EncodedBlobEncoded(pub Vec<Scalar>); // BLOB_ROW_N2 * BLOB_COL_N
 
 impl Deref for EncodedBlobEncoded {
-    type Target = Box<[[Scalar; BLOBN]; BLOBN * 2]>;
+    type Target = Vec<Scalar>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -84,10 +87,10 @@ impl DerefMut for EncodedBlobEncoded {
 }
 
 #[repr(transparent)]
-pub struct RowCommitments(pub [G1Affine; BLOBN]); // num_rows_origin
+pub struct RowCommitments(pub Vec<G1Affine>); // BLOB_ROW_N2
 
 impl Deref for RowCommitments {
-    type Target = [G1Affine; BLOBN];
+    type Target = Vec<G1Affine>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
@@ -114,13 +117,13 @@ impl Deref for SimulateSetupG1 {
         &self.0
     }
 }
-pub const KZGSETUPN: usize = BLOBN;
+pub const KZG_SETUP_N: usize = 1 << 11;
 impl SimulateSetupG1 {
     pub fn sim_load() -> Self {
         use ark_std::rand::thread_rng;
         let mut rng = thread_rng();
         let s: Scalar = Scalar::rand(&mut rng);
-        let all_s = [s; KZGSETUPN];
+        let all_s = [s; KZG_SETUP_N];
         let setup_g1: Vec<G1Affine> = all_s
             .iter()
             .scan(Scalar::one(), |state, &x| {
@@ -133,30 +136,14 @@ impl SimulateSetupG1 {
     }
 }
 
-#[repr(transparent)]
-pub struct RowCommitmentsEncoded(pub [G1Affine; BLOBN * 2]); // num_rows_encoded
 
-impl Deref for RowCommitmentsEncoded {
-    type Target = [G1Affine; BLOBN * 2];
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for RowCommitmentsEncoded {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-pub struct EncodedBlob {
-    encoded: EncodedBlobEncoded,
-    row_commitments_encoded: RowCommitmentsEncoded,
-    row_coeff: [Scalar; BLOBN],
-    da_commitment: G1Affine,
-    row_merkle_roots: [H256; BLOBN],
-    data_root: H256,
+pub struct EncodedBlobKZG {
+    pub encoded: EncodedBlobEncoded,
+    pub row_commitments: RowCommitments,
+    pub da_commitment: G1Affine,
+    pub da_proofs: Vec<G1Affine>
+    //row_merkle_roots: Vec<H256>, // BLOB_ROW_N
+    //data_root: H256,
 }
 
 // pub struct  EncodedSlice {
